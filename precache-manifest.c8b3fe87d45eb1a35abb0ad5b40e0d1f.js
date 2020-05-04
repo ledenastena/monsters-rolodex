2558908a029494fa28c7cd43728368842a766d53
Milan <kockastajabuka@gmail.com>
2020-05-04 13:36:06 +0200

    Updates

---
+++ precache-manifest.c8b3fe87d45eb1a35abb0ad5b40e0d1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ff0c1845cf6b4c8bb57ccd185dd87c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5f0a24d9459e19a922c2",
+    "url": "/monsters-rolodex/static/css/main.c01bd5a7.chunk.css"
+  },
+  {
+    "revision": "1a4d155a13d43d012742",
+    "url": "/monsters-rolodex/static/js/2.9f341a9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9f341a9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0a24d9459e19a922c2",
+    "url": "/monsters-rolodex/static/js/main.2d4c0ab6.chunk.js"
+  },
+  {
+    "revision": "1f1dce36aed9ef7f1a1a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db967828.js"
+  }
+]);
